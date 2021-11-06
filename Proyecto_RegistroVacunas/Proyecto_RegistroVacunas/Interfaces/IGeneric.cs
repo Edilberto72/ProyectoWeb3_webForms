a87ae09d55c0f29094b836a2bb300713621c6b62
JhonatanGabrielPenarandaAndia <gabriel24332@hotmail.com>
2021-11-06 17:42:47 -0400

    Añadiendo interfaces, tambien creando dos modelos de usuario y vacunacion registro, implementando sus respectivos metodos de Save(Insert) y logrando insertar datos a la bdd

---
+++ Proyecto_RegistroVacunas/Proyecto_RegistroVacunas/Interfaces/IGeneric.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_RegistroVacunas.Interfaces
+{
+    interface IGeneric<T>
+    {
+        List<T> GetAll();
+
+        void Save(T t);
+
+        void Update(T t);
+
+        void Delete(T t);
+    }
+}
---
+++ Proyecto_RegistroVacunas/Proyecto_RegistroVacunas/Interfaces/IUser.cs
@@ -0,0 +1,13 @@
+﻿using Proyecto_RegistroVacunas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_RegistroVacunas.Interfaces
+{
+    interface IUser: IGeneric<User>
+    {
+    }
+}
