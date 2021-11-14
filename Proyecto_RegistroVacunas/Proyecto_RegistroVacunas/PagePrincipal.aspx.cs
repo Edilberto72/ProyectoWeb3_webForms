bdb5e772274573754380dd058f4e3e090bedcf22
JhonatanGabrielPenarandaAndia <gabriel24332@hotmail.com>
2021-11-14 16:14:31 -0400

    Redireccionando de la pagina principal

--- Proyecto_RegistroVacunas/Proyecto_RegistroVacunas/PagePrincipal.aspx.cs
+++ Proyecto_RegistroVacunas/Proyecto_RegistroVacunas/PagePrincipal.aspx.cs
@@ -23,12 +23,26 @@ namespace Proyecto_RegistroVacunas
 
         protected void btnPersonalLogin_Click(object sender, EventArgs e)
         {
-            Response.Redirect("PersonalLogin.aspx");
+            try
+            {
+                Response.Redirect("PersonalLogin.aspx");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
         }
 
         protected void btnRegisterAppointment_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                Response.Redirect("RegisterAppointmentTime.aspx");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
         }
 
         protected void btnLocation_Click(object sender, EventArgs e)
@@ -41,14 +55,28 @@ namespace Proyecto_RegistroVacunas
 
         }
 
-        protected void btnAppointmentManager_Click(object sender, EventArgs e)
-        {
-
-        }
-
         protected void btnVaccineRegistration_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Response.Redirect("RegisterVaccinations.aspx");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
 
+        protected void btnVaccineConfirmation_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Response.Redirect("VaccineConfirmation.aspx");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
         }
     }
 }
